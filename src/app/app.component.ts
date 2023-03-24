c014ac492b052d68d23d83b800cfdd3d1fed57c0
Harshad <harshadkhandare9000@gmail.com>
2023-03-24 21:29:24 +0530

    Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
+import { NgTinyUrlService } from 'ng-tiny-url';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'url-shortener';
+  model = {
+    inputUrl: '',
+  };
+  isFormSubmitted = false;
+  shortUrl = '';
+  isTextCopied = false;
+  isLoading = false;
+  constructor(private tinyUrlService: NgTinyUrlService) {}
+
+  onSubmitUrlForm(urlForm: NgForm) {
+    if (urlForm.valid) {
+      this.isLoading = true;
+      this.tinyUrlService.shorten(this.model.inputUrl).subscribe(
+        (data) => {
+          this.shortUrl = data;
+          this.isFormSubmitted = true;
+          this.isLoading = false;
+        },
+        (error) => {
+          alert('Something Went Wrong. Please check your url and try again');
+          this.isLoading = false;
+        }
+      );
+    }
+  }
+
+  copyUrl(shortUrlElementRef: Element) {
+    let inputElement = document.createElement('input');
+
+    inputElement.setAttribute('type', 'text');
+    inputElement.setAttribute('value', shortUrlElementRef.innerHTML);
+
+    inputElement.select();
+    inputElement.setSelectionRange(0, 999999); // For Mobile Selection
+    try {
+      navigator.clipboard.writeText(inputElement.value);
+
+      this.isTextCopied = true;
+
+      setTimeout(() => {
+        this.isTextCopied = false;
+      }, 2000);
+    } catch (e) {
+      console.log('error while copying..');
+    }
+  }
+
+  reset() {
+    this.model.inputUrl = '';
+    this.isFormSubmitted = false;
+    this.isTextCopied = false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule }   from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
+import { NgTinyUrlModule } from 'ng-tiny-url';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    NgTinyUrlModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
